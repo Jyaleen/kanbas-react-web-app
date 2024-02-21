71752129bab02f7527feac0129d7e0f7dc3076cb
jyaleenwu <wu.jy@northeastern.edu>
2024-02-21 18:42:39 -0500

    lab done

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import logo from './logo.svg';
 // import './App.css';
 import Labs from './Labs';
 import Kanbas from './Kanbas';
-import HelloWorld from './HelloWorld';
+import HelloWorld from "./Labs/a3/HelloWorld";
+import { HashRouter, Link } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router";
 
 function App() {
   return (
-    <>
-      <Labs />
-      <HelloWorld />
-      <Kanbas />
-    </>);
+    <HashRouter>
+      <>
+        <Routes>
+          <Route path="/" element={<Navigate to="/Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+          <Route path="/hello" element={<HelloWorld />} />
+        </Routes>
+      </>
+    </HashRouter>);
 }
 
 export default App;
