709a9d0307400be3b132c43e14c631ec75d0ca84
jyaleenwu <wu.jy@northeastern.edu>
2024-03-20 04:19:18 -0400

    up to 2.4

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,10 +1,50 @@
-function Assignment4() {
-    return (
-        <div>
-            <h2>Assignment 4</h2>
-            <h3>React</h3>
-        </div>
-    )
-}
+import React from "react";
+import ArrayStateVariable from "./ArrayStateVariable";
+import BooleanStateVariables from "./BooleanStateVariables";
+import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariable";
+import EventObject from "./EventObject";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import StringStateVariables from "./StringStateVariables";
 
+function Assignment4() {
+    function sayHello() {
+        alert("Hello");
+    }
+    return (
+        <>
+            <h1>Assignment 4</h1>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <ParentStateComponent />
+            <br />
+            <ArrayStateVariable />
+            <br />
+            <ObjectStateVariable />
+            <br />
+            <DateStateVariable />
+            <br />
+            <StringStateVariables />
+            <br />
+            <BooleanStateVariables />
+            <br />
+            <Counter />
+            <br />
+            <EventObject />
+            <br />
+            <PassingFunctions theFunction={sayHello} />
+            <br />
+            <PassingDataOnEvent />
+            <br />
+            <ClickEvent />
+        </>
+    );
+};
 export default Assignment4;
