9754f7e77fb9a56f2dd80afb5dab10221aa56e7a
jyaleenwu <wu.jy@northeastern.edu>
2024-04-03 22:59:49 -0400

    a5

--- kanbas-node-server-app/App.js
+++ kanbas-node-server-app/App.js
@@ -2,11 +2,13 @@ import express from "express";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import Hello from "./Hello.js"
 import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
+Hello(app);
 Lab5(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
